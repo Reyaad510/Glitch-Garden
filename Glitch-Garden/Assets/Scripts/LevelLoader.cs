e625f00a959ac2a04717e99076264796f8b854bd
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-04 20:31:34 -0500

    Fixed some aspect ratio problems

---
+++ Glitch-Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    [SerializeField] int delayInSeconds = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SceneManager method is GetActiveScene().buildIndex
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        // Scene Manager method called LoadScene
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
