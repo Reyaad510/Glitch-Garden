1d64663a7cd9abeb9ae39338f53a29cdd0e70a2c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-05 19:30:14 -0500

    Cactus now snaps into specfic grid when clicked

--- Glitch-Garden/Assets/Scripts/DefenderSpawner.cs
+++ Glitch-Garden/Assets/Scripts/DefenderSpawner.cs
@@ -18,12 +18,21 @@ public class DefenderSpawner : MonoBehaviour
         
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y); // where we click
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos); // converting position to world to fit camera
-        return worldPos;
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
     }
 
-    private void SpawnDefender(Vector2 worldPos)
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 roundedPos)
     {
        
-        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+        GameObject newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as GameObject;
+        Debug.Log(roundedPos);
     }
 }
