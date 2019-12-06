fe212c936715b03eb5209e4fe650042c045a8be5
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-05 20:53:07 -0500

    Implemented UI bar that has cactus and trophy icons for user to click. Implemented code when one clicked will toggle white. Only one can be toggled white at a time

---
+++ Glitch-Garden/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+
+
+    private void OnMouseDown()
+    {
+        // looking for every object that has the defenderbutton script then puts them in a collection that you can do a foreach loop in
+        // This controls the defenderbuttons to toggle from white to black(can only have one be white to show user what is selected that will be placed)
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(43, 43, 43, 255); // new Color32 means the rbg values and the three 43 refers to the grayed out color
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
