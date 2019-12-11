bd73630bd628adbd3da20b15b508b9514a677602
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-11 15:37:04 -0500

    Implemented Level Win screen and music for it

--- Glitch-Garden/Assets/Scripts/LevelController.cs
+++ Glitch-Garden/Assets/Scripts/LevelController.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-
+    [SerializeField] GameObject winLabel;
+    [SerializeField] float waitToLoad = 4f;
     int numberOfAttackers = 0;
     bool levelTimerFinished = false;
+
+
+
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+    }
 
 
     public void AttackerSpawned()
@@ -21,9 +30,20 @@ public class LevelController : MonoBehaviour
         numberOfAttackers--;
         if( numberOfAttackers <=0 && levelTimerFinished)
         {
-            Debug.Log("End Level!");
+            StartCoroutine(HandleWinCondition());
         }
     }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        Debug.Log("Playing Music");
+
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
 
     public void LevelTimerFinished()
     {
