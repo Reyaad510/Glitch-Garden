247be0ad29bf9a630c1d8f30cd089817f7ef2b06
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-12 14:22:13 -0500

    Fixed bug where enemy wasnt being destroyed when hitting collider if went pass defenders

--- Glitch-Garden/Assets/Scripts/DamageCollider.cs
+++ Glitch-Garden/Assets/Scripts/DamageCollider.cs
@@ -7,9 +7,10 @@ public class DamageCollider : MonoBehaviour
 
 
 
-   private void OnTriggerEnter2D()
+   private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         FindObjectOfType<Lives>().TakeLife();
+        Destroy(otherCollider.gameObject);
     }
 
 }
